47608d44dfaa816d73abe587fb816d0ccd5cb4b4
Tanish goyal <goyaltanish08@gmail.com>
2026-03-20 12:04:08 +0530

    Fix CSRF settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'cloudinary',
 ]
 
-import os
+
 import cloudinary
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'dfr142qow'),
@@ -72,7 +73,7 @@ CSRF_COOKIE_SECURE = False
 
 ROOT_URLCONF = 'backend.urls'
 
-import os
+
 
 TEMPLATES = [
     {
@@ -150,9 +151,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-import os
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'static')]
-import os
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
