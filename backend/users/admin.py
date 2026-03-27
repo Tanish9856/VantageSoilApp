590fd537c445330fc2dac1fa430693678d082e0a
Tanish goyal <goyaltanish08@gmail.com>
2026-03-27 11:12:44 +0530

    Fix about page and location error

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,19 +1,69 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.utils.html import format_html
-from .models import SoilScan, UserProfile
+from soil.models import SoilScan, UserProfile
+
+
+# Show SoilScans inline under each user
+class SoilScanInline(admin.TabularInline):
+    model = SoilScan
+    extra = 0
+    readonly_fields = ('soil_type', 'ph_value', 'moisture', 'crop', 'date', 'image')
+    fields = ('soil_type', 'ph_value', 'moisture', 'crop', 'date', 'image')
+    can_delete = False
+    show_change_link = True
+
+
+# Show UserProfile (city, mobile) inline under each user
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile (City & Mobile)'
+    fields = ('mobile', 'city')
+
+
+# Extend the default UserAdmin
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, SoilScanInline)
+    list_display = ('username', 'email', 'first_name', 'get_city', 'get_mobile', 'date_joined')
+
+    def get_city(self, obj):
+        try:
+            return obj.userprofile.city
+        except:
+            return '-'
+    get_city.short_description = 'City'
+
+    def get_mobile(self, obj):
+        try:
+            return obj.userprofile.mobile
+        except:
+            return '-'
+    get_mobile.short_description = 'Mobile'
+
+
+# Re-register User with custom admin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
 
 @admin.register(SoilScan)
 class SoilScanAdmin(admin.ModelAdmin):
-    # This list determines what columns you see in the main table
-    list_display = ('soil_type', 'user_name', 'date', 'view_on_map')
-    
-    # This function creates the clickable link
-    def view_on_map(self, obj):
+    list_display = ('user', 'soil_type', 'ph_value', 'moisture', 'date', 'open_in_maps')
+    list_filter = ('soil_type', 'date')
+    search_fields = ('user__username', 'soil_type')
+    readonly_fields = ('date', 'open_in_maps')
+
+    def open_in_maps(self, obj):
         if obj.latitude and obj.longitude:
             url = f"https://www.google.com/maps?q={obj.latitude},{obj.longitude}"
-            return format_html('<a href="{}" target="_blank">📍 Open in Maps</a>', url)
-        return "No Location"
-    
-    view_on_map.short_description = "Google Maps"
-
-admin.site.register(UserProfile)
+            return format_html(
+                '<a href="{}" target="_blank" style="'
+                'background:#1a5c2a; color:#fff; padding:4px 10px; '
+                'border-radius:6px; text-decoration:none; font-size:12px;">'
+                '📍 Open in Maps</a>',
+                url
+            )
+        return "No location"
+    open_in_maps.short_description = 'Location'
